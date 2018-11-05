df6ffef8adf0c854ae55a6b84080b9a36582d450
runcan24 <44062093+runcan24@users.noreply.github.com>
2018-11-05 17:15:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thibault
- * Date: 05/11/18
- * Time: 15:30
- */
-print "Salut c'est un test";
-print "OK, cool.";
-print "test bien";
+echo "test";
 
 ?>
