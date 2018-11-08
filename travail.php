758705835aff16565bc3094b16b75ff737065fa6
walkerX19 <walkerX19@laposte.net>
2018-11-08 11:48:49 +0100

    Merge branch 'master' of https://github.com/thibe19/PPE3

---
+++ travail.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Viabahuet</title>
+  </head>
+  <body>
+<h4>MCD : Fait</h4>
+<h4>BDD :  oui mais non en fait //Fait</h4>
+<h2>class</h2>
+<ul>
+  <li>Utilisateur: Heritage Eleve est Entreprise / Crée, modifer, Supp / Diplome</li>
+  <li>Evenement: Crée, modifer, supp / Type</li>
+  <li>Post: Crée, modifer, supp / Tag</li>
+  <li>Offre: Heritage Ostage est Oemploi / Crée, modifer, supp / Tag</li>
+
+</ul>
+  </body>
+</html>
