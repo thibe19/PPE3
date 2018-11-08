e1efabe88ce8e2d17ed73d07ab797da5a0ed87ac
thibault <thibaultnesti@gmail.com>
2018-11-08 17:51:18 +0100

    Commentaire et modifications 08/11/18

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
+//Test si l'identifiant est déjà connecté
+//Si oui il le renvoie vers le dite
 if(isset($_SESSION['email']) && isset($_SESSION['mdp'])){
   header("Location: ./template");
 }
+//Sinon il sera renvoyé vers la page de connection.
 else {
   header("Location: ./login_page");
 }
