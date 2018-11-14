51e6f19891a2b3dd24117afa2c306bd9e85b5ced
runcan24 <yannther99@gmail.com>
2018-11-14 15:02:32 +0100

    yann 15h02

--- login_page/Register.php
+++ login_page/Register.php
@@ -62,7 +62,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Champ obligatoire">
-						<input class="input100" type="password" name="pass" placeholder="Mot de passe">	<!-- Champ inscription Mot de passe  -->
+						<input class="input100" type="password" id="pass_n" name="pass" value="" placeholder="Mot de passe">	<!-- Champ inscription Mot de passe  -->
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -70,7 +70,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Champ obligatoire">
-						<input class="input100" type="password" name="pass_verification" placeholder="Vérification Mot de passe"> <!-- Champ inscription Vérification de mot passe  -->
+						<input class="input100" type="password" id="pass_v" name="pass_verification" value="" placeholder="Vérification Mot de passe"> <!-- Champ inscription Vérification de mot passe  -->
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -80,19 +80,23 @@
 
 
 					<!-- <form action="#" method="post"> -->
-						<div class="container-login100-form-btn">	<!-- Bouton accer prochaine étape ///////////: name="E1" value="1" -->
-							<button onclick="checkPw(pass,pass_verification)" class="login100-form-btn">
+						<div class="container-login100-form-btn">	<!-- Bouton accer prochaine étape /////////// name="E1" value="1" -->
+							<button onclick="checkPw()" class="login100-form-btn">
 								Prochaine étape
 							</button>
 						</div>
 					<!-- </form> -->
 
 					<script type="text/javascript">
-						function checkPw(pass,pass_verification) {
-							var pass = name.pass.value;
+						function checkPw() {
+							var pass = document.getElementById("pass_n").value;
+							var pass_v = document.getElementById("pass_v").value;
+							if (pass != pass_v) {
+								alert("pas égaux");
+							}
+							else {
 
-							console.log(pass);
-
+							}
 						}
 					</script>
 
