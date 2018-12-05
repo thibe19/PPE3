60edf659a99bdc3a68dcd7c4799a8f7de9757207
walkerX19 <walkerX19@laposte.net>
2018-12-05 13:52:01 +0100

    index

--- template/index.php
+++ template/index.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- *  28/11/18
- *  index-2
- *  v0.0.2
+ *  05/11/18
+ *  index
+ *  v0.0.3
  */
 session_start();
 
@@ -181,7 +181,7 @@ require('part/header.php');
                             <div class="post_content">
                                 <a href="details.html" class="post_img">
                                     <img src="images/post.jpg" alt="">
-                                    <span><i class="ion-android-radio-button-off"></i><?php echo$dataC['lib_cat']; ?></span>
+                                    <span><i class="ion-android-radio-button-off"></i><?php echo $dataC['lib_cat']; ?></span>
                                 </a>
                                 <div class="row author_area">
                                     <div class="col s4 author">
@@ -189,8 +189,8 @@ require('part/header.php');
 
                                         <div class="col s8 media_body">
 
-                                            <a href="#"><?php echo$dataU['nom_user']; ?></a>
-                                            <span><?php echo$dataP['date_post']; ?>,<?php echo$dataP['heure_post']; ?></span>
+                                            <a href="#"><?php echo urldecode($dataU['nom_user']); ?></a>
+                                            <span><?php echo $dataP['date_post']; ?>,<?php echo$dataP['heure_post']; ?></span>
                                         </div>
                                     </div>
                                     <div class="col s4 btn_floating">
@@ -198,8 +198,8 @@ require('part/header.php');
                                     </div>
 
                                 </div>
-                                <a  class="post_heding"><?php echo$dataP['titre_post']; ?></a>
-                                <p><?php echo$dataP['contenu_post']; ?></p>
+                                <a  class="post_heding"><?php echo urldecode($dataP['titre_post']); ?></a>
+                                <p><?php echo urldecode($dataP['contenu_post']); ?></p>
                             </div>
                             <center><a href="#" class="btn-floating waves-effect"><i class="ion-navicon-round"></i></a></center>
                             <br>
@@ -222,22 +222,22 @@ require('part/header.php');
 
                     <?php
 
-                    $sqlS="SELECT * FROM Offre ";
-                    $resS = $conn -> query($sqlS)or die($conn -> errorInfo());
+                    $sqlO="SELECT * FROM Offre ";
+                    $resO = $conn -> query($sqlO)or die($conn -> errorInfo());
 
-                    while ($dataS=$resS->fetch())
+                    while ($dataO=$resO->fetch())
                     {
-                        $offre = $dataS['id_offre'];
-                        $sqlO="SELECT * FROM OStage WHERE id_offre = '$offre' ";
-                        $resO = $conn -> query($sqlO)or die($conn -> errorInfo());
-                        $dataO=$resO->fetch();
+                        $offre = $dataO['id_offre'];
+                        $sqlS="SELECT * FROM OStage WHERE id_offre = '$offre' ";
+                        $resS = $conn -> query($sqlS)or die($conn -> errorInfo());
+                        $dataS=$resS->fetch();
 
-                        $cat = $dataS['id_cat'];
+                        $cat = $dataO['id_cat'];
                         $sqlC="SELECT * FROM Categorie WHERE id_cat = '$cat' ";
                         $resC = $conn -> query($sqlC)or die($conn -> errorInfo());
                         $dataC=$resC->fetch();
 
-                        $id_user= $dataS['id_user'];
+                        $id_user= $dataO['id_user'];
                         $sqlU="SELECT * FROM Utilisateur WHERE id_user = '$id_user'";
                         $resU = $conn -> query($sqlU)or die($conn -> errorInfo());
                         $dataU = $resU -> fetch();
@@ -256,16 +256,16 @@ require('part/header.php');
 
                                         <div class="col s8 media_body">
 
-                                            <a href="#"><?php echo$dataU['nom_user']; ?></a>
-                                            <span><?php echo$dataS['date_post_offre']; ?></span>
+                                            <a ><?php echo urldecode($dataO['date_post_offre']); ?></a>
+
                                         </div>
                                     </div>
                                     <div class="col s4 btn_floating">
 
                                     </div>
                                 </div>
-                                <a class="post_heding"><?php echo$dataS['lib_offre']; ?></a>
-                                <p><?php echo$dataO['desc_user_stage']; ?></p>
+                                <a class="post_heding"><?php echo urldecode($dataO['lib_offre']); ?></a>
+                                <p><?php echo urldecode($dataO['desc_offre']); ?></p>
                             </div>
                             <center><a href="#" class="btn-floating waves-effect"><i class="ion-navicon-round"></i></a></center>
                             <br>
@@ -642,110 +642,30 @@ require('part/header.php');
                 <h3 class="categories_tittle">Popular Posts</h3>
                 <div class="row valign-wrapper popular_item">
                     <div class="col s3 p_img">
-                        <a href="#">
+                       <a href="#">
                             <img src="images/recent-post-1.jpg" alt="" class="circle responsive-img">
-                        </a>
+                       </a>
                     </div>
                     <div class="col s9 p_content">
-                        <a href="#">Poster can be one of the <br> effective marketing and </a>
+                       <a href="#">Poster can be one of the <br> effective marketing and </a>
                         <span class="black_text">2 days ago</span>
                     </div>
                 </div>
                 <div class="row valign-wrapper popular_item">
                     <div class="col s3 p_img">
-                        <a href="#">
+                       <a href="#">
                             <img src="images/recent-post-2.jpg" alt="" class="circle responsive-img">
-                        </a>
+                       </a>
                     </div>
                     <div class="col s9 p_content">
-                        <a href="#">Color is so powerful that it can persuade, motivate, inspire</a>
+                       <a href="#">Color is so powerful that it can persuade, motivate, inspire</a>
                         <span class="black_text">3 days ago</span>
                     </div>
                 </div>
-                <div class="row valign-wrapper popular_item">
-                    <div class="col s3 p_img">
-                        <a href="#">
-                            <img src="images/recent-post-3.jpg" alt="" class="circle responsive-img">
-                        </a>
-                    </div>
-                    <div class="col s9 p_content">
-                        <a href="#">What makes one logo better than another?</a>
-                        <span class="black_text">4 days ago</span>
-                    </div>
-                </div>
             </div>
         </div>
-        <div class="col l3 m6 footer_col footer_trending">
-            <h3 class="categories_tittle">Trending</h3>
-            <div class="trending_area">
-                <ul class="collapsible trending_collaps" data-collapsible="accordion">
-                    <li>
-                        <div class="collapsible-header"><i class="ion-chevron-right"></i>Healthy Environment For Self Esteem</div>
-                        <div class="collapsible-body">
-                            <div class="row collaps_wrpper">
-                                <div class="col s1 media_l">
-                                    <b>1</b>
-                                    <i class="ion-android-arrow-dropup-circle"></i>
-                                </div>
-                                <div class="col s11 media_b">
-                                    <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                    <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem</p>
-                                    <h6>By <a href="#">Thomas Omalley</a></h6>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="collapsible-header"><i class="ion-chevron-right"></i>Burn The Ships</div>
-                        <div class="collapsible-body">
-                            <div class="row collaps_wrpper">
-                                <div class="col s1 media_l">
-                                    <b>1</b>
-                                    <i class="ion-android-arrow-dropup-circle"></i>
-                                </div>
-                                <div class="col s11 media_b">
-                                    <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                    <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem</p>
-                                    <h6>By <a href="#">Thomas Omalley</a></h6>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="collapsible-header active"><i class="ion-chevron-right"></i>Harness The Power Of Your Dreams</div>
-                        <div class="collapsible-body">
-                            <div class="row collaps_wrpper">
-                                <div class="col s1 media_l">
-                                    <b>1</b>
-                                    <i class="ion-android-arrow-dropup-circle"></i>
-                                </div>
-                                <div class="col s11 media_b">
-                                    <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                    <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem</p>
-                                    <h6>By <a href="#">Thomas Omalley</a></h6>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col l3 m6 footer_col">
-            <div class="badges">
-                <h3 class="categories_tittle">Badges</h3>
-                <ul class="badges_list">
-                    <li><a href="#"><i class="ion-bonfire"></i><span>6</span></a></li>
-                    <li><a href="#"><i class="ion-bluetooth"></i></a></li>
-                    <li><a href="#"><i class="ion-coffee"></i></a></li>
-                    <li><a href="#"><i class="ion-clock"></i> <span>3</span></a></li>
-                    <li><a href="#"><i class="ion-camera"></i></a></li>
-                    <li><a href="#"><i class="ion-ios-bell-outline"></i><span>2</span></a></li>
-                    <li><a href="#"><i class="ion-bluetooth"></i></a></li>
-                    <li><a href="#"><i class="ion-coffee"></i></a></li>
-                    <li><a href="#"><i class="ion-clock"></i></a></li>
-                </ul>
-            </div>
 
+        <div class="col l3 m6 footer_col">
             <div class="social_Sharing">
                 <h3 class="categories_tittle">Social Sharing</h3>
                 <ul class="social_icon">
@@ -757,12 +677,9 @@ require('part/header.php');
                 </ul>
             </div>
         </div>
-        <div class="col l3 m6 footer_col">
-            <img src="images/advertis-3.jpg" alt="" class="responsive-img">
-        </div>
     </div>
     <div class="copy_right">
-        � 2018 <a href="#">Open List</a>. All rights reserved.
+        © 2018 <a href="#">Open List</a>. All rights reserved.
     </div>
 </footer>
 <!-- End Footer area -->
