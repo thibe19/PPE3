e16ee71b87a777e4b6be81de8aa395fd9c18f23e
runcan24 <yannther99@gmail.com>
2018-12-05 09:09:22 +0100

    05/12/2018

--- template/profile.php
+++ template/profile.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['Eleve'])) {
 }
 
 
-$_SESSION['Profilon']=1;
+
 //TODO PENSER A DESCTIVER LA SESSION Profilon quand navigation en dehors d'une page profil
 ?>
 <!DOCTYPE html>
@@ -75,15 +75,15 @@ $_SESSION['Profilon']=1;
         <div class="row author_profile_row">
             <div class="col l4 m6">
                 <ul class="profile_menu">
-                    <li><a href="profile.html">Activiter</a></li>
+                    <li><a href="profile.php">Activiter</a></li>
                     <li><a href="about.php">A propos</a></li>
-                    <li class="post_d"><a class="dropdown-button" href="#!" data-activates="dro_pm">...</a>
+                    <!-- <li class="post_d"><a class="dropdown-button" href="#!" data-activates="dro_pm">...</a> -->
                         <!-- Dropdown Structure -->
-                        <ul id="dro_pm" class="dropdown-content">
+                        <!-- <ul id="dro_pm" class="dropdown-content">
                             <li><a href="#">Popular Post</a></li>
                             <li><a href="#">Save Post</a></li>
                         </ul>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
             <div class="col l4 m6">
@@ -184,209 +184,8 @@ $_SESSION['Profilon']=1;
                     </div>
                     <?php require ('./part/left.php')?>
             <!-- Right side bar -->
-            <div class="right_side_bar col">
-                <div class="right_sidebar_iner">
-                    <div class="popular_posts popular_fast">
-                        <h3 class="categories_tittle">My Submisstion</h3>
-                        <div class="row valign-wrapper popular_item">
-                            <div class="col s3 p_img">
-                               <a href="#">
-                                    <img src="images/recent-post-1.jpg" alt="" class="circle responsive-img">
-                               </a>
-                            </div>
-                            <div class="col s9 p_content">
-                               <a href="#">You submitted a new photo  to <span>How To Talk With Girls</span></a>
-                                <span class="black_text">2 days ago</span>
-                            </div>
-                        </div>
-                        <div class="row valign-wrapper popular_item">
-                            <div class="col s3 p_img">
-                               <a href="#">
-                                    <img src="images/recent-post-2.jpg" alt="" class="circle responsive-img">
-                               </a>
-                            </div>
-                            <div class="col s9 p_content">
-                               <a href="#">You contributed a new paragraph to <span>10 Ways To Make Easy Suicide</span></a>
-                                <span class="black_text">3 days ago</span>
-                            </div>
-                        </div>
-                        <div class="row valign-wrapper popular_item">
-                            <div class="col s3 p_img">
-                               <a href="#">
-                                    <img src="images/recent-post-3.jpg" alt="" class="circle responsive-img">
-                               </a>
-                            </div>
-                            <div class="col s9 p_content">
-                               <a href="#">You submitted 10 photos  to <span>Best Photos of The Tech Giants</span></a>
-                                <span class="black_text">4 days ago</span>
-                            </div>
-                        </div>
-                        <div class="row valign-wrapper popular_item">
-                            <div class="col s3 p_img">
-                               <a href="#">
-                                    <img src="images/recent-post-4.jpg" alt="" class="circle responsive-img">
-                               </a>
-                            </div>
-                            <div class="col s9 p_content">
-                               <a href="#">You submitted a new photo  to <span>How To Talk With Girls</span></a>
-                                <span class="black_text">5 days ago</span>
-                            </div>
-                        </div>
-                        <div class="row valign-wrapper popular_item">
-                            <div class="col s3 p_img">
-                               <a href="#">
-                                    <img src="images/recent-post-5.jpg" alt="" class="circle responsive-img">
-                               </a>
-                            </div>
-                            <div class="col s9 p_content">
-                               <a href="#">You contributed a new paragraph to <span>10 Ways To Make Easy Suicide</span></a>
-                                <span class="black_text">10 days ago</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="popular_gallery row">
-                        <h3 class="categories_tittle">Images</h3>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-1.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-2.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-3.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-4.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-5.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-6.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-7.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-8.jpg" alt=""></a></div>
-                        <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-9.jpg" alt=""></a></div>
-                    </div>
-                    <div class="trending_area">
-                        <h3 class="categories_tittle">Trending</h3>
-                        <ul class="collapsible trending_collaps" data-collapsible="accordion">
-                            <li>
-                                <div class="collapsible-header"><i class="ion-chevron-right"></i>Healthy Environment For Self Esteem</div>
-                                <div class="collapsible-body">
-                                    <div class="row collaps_wrpper">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                    <div class="row collaps_wrpper collaps_2">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header"><i class="ion-chevron-right"></i>Burn The Ships</div>
-                                <div class="collapsible-body">
-                                    <div class="row collaps_wrpper">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                    <div class="row collaps_wrpper collaps_2">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header active"><i class="ion-chevron-right"></i>Harness The Power Of Your Dreams</div>
-                                <div class="collapsible-body">
-                                    <div class="row collaps_wrpper">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                    <div class="row collaps_wrpper collaps_2">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header"><i class="ion-chevron-right"></i>Don T Let The Outtakes Take You Out</div>
-                                <div class="collapsible-body">
-                                    <div class="row collaps_wrpper">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                    <div class="row collaps_wrpper collaps_2">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="collapsible-header"><i class="ion-chevron-right"></i>Helen Keller A Teller And A Seller</div>
-                                <div class="collapsible-body">
-                                    <div class="row collaps_wrpper collaps_2">
-                                        <div class="col s1 media_l">
-                                            <b>1</b>
-                                            <i class="ion-android-arrow-dropdown-circle"></i>
-                                        </div>
-                                        <div class="col s11 media_b">
-                                            <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
-                                            <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
-                                            <h6>By <a href="#">Thomas Omalley</a></h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+                    <?php require ('./part/right.php')?>
+            <!-- Right bar end -->
         </div>
     </section>
 
@@ -422,15 +221,15 @@ $_SESSION['Profilon']=1;
             <div class="row author_profile_row">
                 <div class="col l4 m6">
                     <ul class="profile_menu">
-                        <li><a href="profile.html">Activiter</a></li>
+                        <li><a href="profile.php">Activiter</a></li>
                         <li><a href="about.php">A propos</a></li>
-                        <li class="post_d"><a class="dropdown-button" href="#!" data-activates="dro_pm">...</a>
+                        <!-- <li class="post_d"><a class="dropdown-button" href="#!" data-activates="dro_pm">...</a>
                             <!-- Dropdown Structure -->
-                            <ul id="dro_pm" class="dropdown-content">
+                            <!-- <ul id="dro_pm" class="dropdown-content">
                                 <li><a href="#">Popular Post</a></li>
                                 <li><a href="#">Save Post</a></li>
                             </ul>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
                 <div class="col l4 m6">
