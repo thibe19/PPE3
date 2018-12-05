d33852a92d6c31a32cbcb08eae8fa119f7a89792
runcan24 <yannther99@gmail.com>
2018-12-05 09:18:43 +0100

    05/12/2018

---
+++ template/part/right.php
@@ -0,0 +1,203 @@
+<div class="right_side_bar col">
+    <div class="right_sidebar_iner">
+        <div class="popular_posts popular_fast">
+            <h3 class="categories_tittle">My Submisstion</h3>
+            <div class="row valign-wrapper popular_item">
+                <div class="col s3 p_img">
+                   <a href="#">
+                        <img src="images/recent-post-1.jpg" alt="" class="circle responsive-img">
+                   </a>
+                </div>
+                <div class="col s9 p_content">
+                   <a href="#">You submitted a new photo  to <span>How To Talk With Girls</span></a>
+                    <span class="black_text">2 days ago</span>
+                </div>
+            </div>
+            <div class="row valign-wrapper popular_item">
+                <div class="col s3 p_img">
+                   <a href="#">
+                        <img src="images/recent-post-2.jpg" alt="" class="circle responsive-img">
+                   </a>
+                </div>
+                <div class="col s9 p_content">
+                   <a href="#">You contributed a new paragraph to <span>10 Ways To Make Easy Suicide</span></a>
+                    <span class="black_text">3 days ago</span>
+                </div>
+            </div>
+            <div class="row valign-wrapper popular_item">
+                <div class="col s3 p_img">
+                   <a href="#">
+                        <img src="images/recent-post-3.jpg" alt="" class="circle responsive-img">
+                   </a>
+                </div>
+                <div class="col s9 p_content">
+                   <a href="#">You submitted 10 photos  to <span>Best Photos of The Tech Giants</span></a>
+                    <span class="black_text">4 days ago</span>
+                </div>
+            </div>
+            <div class="row valign-wrapper popular_item">
+                <div class="col s3 p_img">
+                   <a href="#">
+                        <img src="images/recent-post-4.jpg" alt="" class="circle responsive-img">
+                   </a>
+                </div>
+                <div class="col s9 p_content">
+                   <a href="#">You submitted a new photo  to <span>How To Talk With Girls</span></a>
+                    <span class="black_text">5 days ago</span>
+                </div>
+            </div>
+            <div class="row valign-wrapper popular_item">
+                <div class="col s3 p_img">
+                   <a href="#">
+                        <img src="images/recent-post-5.jpg" alt="" class="circle responsive-img">
+                   </a>
+                </div>
+                <div class="col s9 p_content">
+                   <a href="#">You contributed a new paragraph to <span>10 Ways To Make Easy Suicide</span></a>
+                    <span class="black_text">10 days ago</span>
+                </div>
+            </div>
+        </div>
+        <div class="popular_gallery row">
+            <h3 class="categories_tittle">Images</h3>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-1.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-2.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-3.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-4.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-5.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-6.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-7.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-8.jpg" alt=""></a></div>
+            <div class="col s4 p_img"><a href="#"><img src="images/gallery/gallry-s-9.jpg" alt=""></a></div>
+        </div>
+        <div class="trending_area">
+            <h3 class="categories_tittle">Trending</h3>
+            <ul class="collapsible trending_collaps" data-collapsible="accordion">
+                <li>
+                    <div class="collapsible-header"><i class="ion-chevron-right"></i>Healthy Environment For Self Esteem</div>
+                    <div class="collapsible-body">
+                        <div class="row collaps_wrpper">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                        <div class="row collaps_wrpper collaps_2">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><i class="ion-chevron-right"></i>Burn The Ships</div>
+                    <div class="collapsible-body">
+                        <div class="row collaps_wrpper">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                        <div class="row collaps_wrpper collaps_2">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="collapsible-header active"><i class="ion-chevron-right"></i>Harness The Power Of Your Dreams</div>
+                    <div class="collapsible-body">
+                        <div class="row collaps_wrpper">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                        <div class="row collaps_wrpper collaps_2">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><i class="ion-chevron-right"></i>Don T Let The Outtakes Take You Out</div>
+                    <div class="collapsible-body">
+                        <div class="row collaps_wrpper">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>If you will be traveling for a ski vacation, it is often difficult to know what to pack. You may not even have a problem knowing what to pack, but instead have</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                        <div class="row collaps_wrpper collaps_2">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><i class="ion-chevron-right"></i>Helen Keller A Teller And A Seller</div>
+                    <div class="collapsible-body">
+                        <div class="row collaps_wrpper collaps_2">
+                            <div class="col s1 media_l">
+                                <b>1</b>
+                                <i class="ion-android-arrow-dropdown-circle"></i>
+                            </div>
+                            <div class="col s11 media_b">
+                                <a href="#" class="close_btn"><i class="ion-close-round"></i></a>
+                                <p>The Emerald Buddha is a figurine of a sitting Budha, that is the is the palladium</p>
+                                <h6>By <a href="#">Thomas Omalley</a></h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
