82e4001f687ec5e31223b9c53288dc8b526b574d
walkerX19 <walkerX19@laposte.net>
2018-12-05 16:28:14 +0100

    emp

--- template/oemploi.php
+++ template/oemploi.php
@@ -355,7 +355,7 @@ if (isset($_POST['validOE'])) {
       }
 
       $id_ent = isset($id_ent)?$id_ent:$ent;
-      $unemploi = new Emploi('',$lib_offre,$nreq,$dateDemp,$date,$descemp,'',$cat,$ent,$salaire,$typeC,'','');
+      $unemploi = new Emploi('',$lib_offre,$nreq,$dateDemp,$date,$descemp,'',$cat,$ent,$salaire,$typeC);
       $unemploi->insert_emploi($unemploi,$conn);
 
 
