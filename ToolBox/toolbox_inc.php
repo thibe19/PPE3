c902e92cc48d39abd924e63c9b14fc09a7db6492
thibault <thibaultnesti@gmail.com>
2018-12-10 17:15:16 +0100

    Ajout de la fonction recherche dans la toolbox

--- ToolBox/toolbox_inc.php
+++ ToolBox/toolbox_inc.php
@@ -248,6 +248,41 @@ function update_image($namepho, $login, $photo2, $id_user, $conn) {
   sleep(5);
 }
 
+//Creation d'une requete a partir d'un inout pour la recherche
+function req_recherche($searchs,$tables,$conn){
+    $search_exploded = preg_split ( "/[\s,\/]+/", $searchs );
 
+
+    foreach ($tables as $t => $table) {
+        $req = 'SELECT ';
+        $cond = '';
+        foreach ($table as $champs) {
+            $req .= $champs.', ';
+            $champs = explode(", ",$champs);
+            foreach ($champs as $champ) {
+                if (count($search_exploded) == 1){
+                    $cond .= " $champ LIKE '%$searchs%' OR ";
+                }
+                else{
+                    $i = 0;
+                    foreach( $search_exploded as $search_each ) {
+                        $cond .= "OR $champ LIKE '%$search_each%' ";
+                    }
+                }
+            }
+        }
+        if (count($search_exploded) == 1){
+            $cond = rtrim($cond,'OR ');
+        }
+        else{
+            $cond =ltrim($cond,'OR ');
+        }
+        $req = rtrim($req,', ').' FROM '.$t.' WHERE '.$cond;
+        $result[$t] = reqtoobj($req,$conn);
+    }
+
+
+    return $result;
+}
 
 ?>
