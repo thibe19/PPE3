2c8646ca727bd5bf756b1459ce6fa98f7e9ff6f4
thibault <thibaultnesti@gmail.com>
2019-03-14 11:10:51 +0100

    Mise a jour du 14/03/19

--- objet/class_offre.php
+++ objet/class_offre.php
@@ -351,7 +351,7 @@ class Offre
             </center>
         </div>';
         }
-        if ($_REQUEST['mesposts']){
+        if (isset($_REQUEST['mesposts'])){
             $html = "";
         }
         return $html;
